dab01b4b0ed82fbf0b934725f9959ff208c1763d
Matheus MR <mmussattorodrigues@gmail.com>
2023-01-14 11:57:53 -0300

    VAGA

--- app/Models/Vaga.php
+++ app/Models/Vaga.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Vaga extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titulo',
+        'descricao',
+        'status',
+    ];
 }
